130a951ef5295096358dbe7c6b35306e8d416503
Guy <guydwilks@gmail.com>
2018-09-17 16:46:38 -0700

    heroku ready

--- webDevVer/uploadToJSON.js
+++ webDevVer/uploadToJSON.js
@@ -1,55 +1,119 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-const  http= require('http');
-const sql = require('mssql');
-const hostname = '127.0.0.1';
-const port = 5000;
+var http = require('http');
+var url = require('url');
+var express = require('express');
+var app = express();
+var queryString = require('querystring');
+var request = require('request');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    ///res.setHeader('Content-Type', 'text/plain');
-   // res.end('Hello World\n');
+app.set('port', (process.env.PORT || 5000));
+
+
+// views is directory for all template files
+app.use(express.static(__dirname + '/views/pages'));
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+
+app.get("/authorize", function(req, res) {
+    // Authorization (initiated from your Swift App) is requested at this endpoint
+
+    res.redirect(AUTHORIZE_URI + "?" + queryString.stringify({
+        response_type: "code",
+        scope: "read_write",
+        client_id: CLIENT_ID,
+        force_login: true
+
+    }));
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
 
-// Create connection to database
-var config =
-    {
-        userName: 'guy', // update me
-        password: 'Wilks2003', // update me
-        server: 'winedb.database.windows.net', // update me
-        port: '1433',
-        options:
-            {
-                database: 'winedb' //update me
-                , encrypt: true
+app.get('/redirect', function(req, res){
+
+    //Users are redirected to this endpoint after their request to connect to Stripe is approved.
+
+    var authCode = req.param('code');
+    var scope = req.param('scope');
+    var error = req.param('error');
+    var errorDescription = req.param('error_description');
+    var objectID = req.param('object_id');
+
+
+    if (error) {
+        res.render('pages/fail');
+    } else {
+
+        // Make /oauth/token endpoint POST request
+        request.post({
+            url: TOKEN_URI,
+            form: {
+                grant_type: 'authorization_code',
+                client_id: CLIENT_ID,
+                code: authCode,
+                client_secret: API_KEY
             }
-    }
-var connection = new Connection(config);
+        }, function(err, response, body) {
 
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err)
-    {
-        if (err)
-        {
-            console.log(err)
-        }
-        else
-        {
-            console.log('connected');
-            var request = new sql.Request();
+            if (err) {
+                res.render('pages/fail');
+                return;
+            }
 
-            request.query(
-                "SELECT * FROM wineTest",
-                function(err, rows)
-                {
-                    console.log(rows);
-                }
-            );
-        }
+            // {
+            //   "token_type": "bearer",
+            //   "stripe_publishable_key": PUBLISHABLE_KEY,
+            //   "scope": "read_write",
+            //   "livemode": false,
+            //   "stripe_user_id": USER_ID,
+            //   "refresh_token": REFRESH_TOKEN,
+            //   "access_token": ACCESS_TOKEN
+            // }
+
+            var stripeUserID = JSON.parse(body).stripe_user_id;
+
+            res.render('pages/success', {stripe_user_id: stripeUserID});
+
+        });
     }
-);
+});
+
+app.get('/', function (req, res) {
+
+    var sql = require("mssql");
+
+    // config for your database
+    var config = {
+        user: 'guy',
+        password: 'Wilks2003',
+        server: 'winedb.database.windows.net',
+        database: 'winedb',
+        options: {encrypt: true, database: 'winedb'}
+    };
+
+    // connect to your database
+    sql.connect(config, function (err) {
+
+        if (err) console.log(err);
+
+        // create Request object
+        var request = new sql.Request();
+
+        // query to the database and get the records
+        request.query('SELECT * FROM wineTest', function (err, recordset) {
+
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+
+        });
+    });
+});
+
+http.createServer(function (req, res) {
+    var query = url.parse(req.url,true).query;
+    res.end(JSON.stringify(query.id));
+}).listen(app.get('port'));
 
